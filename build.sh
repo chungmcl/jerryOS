6796ae704dc19ab050c5a5606bc583911bbd39a6
Micheal Chung <chungmcl@chungmcl.com>
2025-06-12 23:33:12 -0700

    removed parsedDeviceTree.dts (unneeded debug output file); cleaned up build.sh and qemu.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 RUSTC_BOOTSTRAP=1
-DISK_DIR=.disks
-DISK_PATH=${DISK_DIR}/disk.img
-
-mkdir -p ${DISK_DIR}
-if [ ! -f "${DISK_PATH}" ]; then echo "\ndisk.img not already present. Creating with qemu-img create:"; qemu-img create -f raw "${DISK_PATH}" 1G; echo "\n"; fi
 
 rustup override set nightly
 rustup component add rust-src
--- qemu.sh
+++ qemu.sh
@@ -1,12 +1,26 @@
 # Uncomment this to print out exactly what this script is running
 # set -x
 
+if ! ./build.sh; then
+  echo "Build failed!"
+  exit 1
+fi
 BUILD_DIR="target/aarch64-unknown-jerryOS-elf"
-DISK_DIR=.disks
-DISK_PATH=${DISK_DIR}/disk.img
-LLDB_PORT=$(sed -nE "s/.*gdb-remote localhost:([0-9]+)/\1/p" ./lldb-startup.txt)
-MEMORY_GB=4
+echo "---------------------------------------"
 
+MEMORY_GB=4
+DISK_GB=2
+DISK_DIR=.disks
+DISK_PATH=${DISK_DIR}/disk_${DISK_GB}G.img
+mkdir -p ${DISK_DIR}
+if [ ! -f "${DISK_PATH}" ]; then 
+  echo ""
+  echo "disk_${DISK_GB}.img not already present. Creating with qemu-img create:"
+  qemu-img create -f raw "${DISK_PATH}" "${DISK_GB}G"
+  echo ""
+fi
+
+LLDB_PORT=$(sed -nE "s/.*gdb-remote localhost:([0-9]+)/\1/p" ./lldb-startup.txt)
 if [ -z "$LLDB_PORT" ]; then
   echo "Error: A valid port in \"gdb-remote localhost:port\" not found in ./lldb-startup.txt!"
   exit 1
@@ -14,9 +28,7 @@ fi
 
 # Note: use "-machine virt,virtualization=on" to enable EL2
 # https://qemu-project.gitlab.io/qemu/system/arm/virt.html
-./build.sh \
-&& \
-echo "Starting QEMU on localhost:$LLDB_PORT with $MEMORY_GB GB of RAM." \
+echo "Starting QEMU on localhost:$LLDB_PORT with $MEMORY_GB GB of RAM & a $DISK_GB GB disk." \
 && \
 qemu-system-aarch64 \
   -machine virt \
