6d3a077fd62b1e65186ab2298817a0a433ffcffa
Micheal Chung <chungmcl@chungmcl.com>
2025-06-16 01:47:58 -0700

    implemented get_bits in bit_tools.py for debugging help; fixed bug where the page table tree wasn't mapped into the kernel's VA space during bootstrap; added notes about how it was properly fixed

--- lldb.sh
+++ lldb.sh
@@ -1,2 +1,1 @@
 rust-lldb -s lldb-startup.txt --no-lldbinit
-# lldb -s lldb-startup.txt --no-lldbinit
--- src/memory/ptm.rs
+++ src/memory/ptm.rs
@@ -30,9 +30,80 @@ pub fn bootstrap_kernel_page_tables(kernel_mem_end: *const u8) -> Result<(), PTM
      * Note that the Kernel's addy range should not be in the "high" range of the VA, i.e.
      * the VA range mapped by $TTBR1_EL1, so we don't do anything to kernelRootTable1.
     */
+
+    /*
+     * When we calculate pages_to_map, we have to calculate pages_needed_for_page_table
+     * and add it to pages_to_map, since we need the table ITSELF mapped into VA space.
+     * Example of what we eventually want to happen:
+     * # In this example, 0  is the highest allocated l1 table index,
+     * #                  32 is the highest allocated l2 table index,
+     * #                  66 is the highest allocated l3 table index.
+     * | *((uint64_t*)(*((uint64_t*)&KERNEL_ROOT_TABLE0[0]) & ~0x03) + 32) & ~0x03
+     * |  = 0x0000000040108000 # address of highest allocated l3 table
+     * 
+     * | *(((uint64_t*)(*((uint64_t*)((*((uint64_t*)&KERNEL_ROOT_TABLE0[0])) & ~0x03) + 32) & ~0x03)) + 66) & ~0x403
+     * |  = 0x0000000040108000 # address of the highest allocated physical page
+     * # We've converged the VA space described by the page table tree, with the PA space INCLUDING
+     * # the page table tree itself!!!
+     * 
+     * Expanded out:
+     * | *(
+     * |     (uint64_t*)(
+     * |         *(
+     * |             (uint64_t*)&KERNEL_ROOT_TABLE0[0]         // Address of the first element of KERNEL_ROOT_TABLE0
+     * |         )                                             // Get the first element of KERNEL_ROOT_TABLE0
+     * |         & ~(0x03)                                     // Ignore TableDescriptorS1.{valid_bit, descriptor_type}
+     * |     )                                                 // Cast first element of KERNEL_ROOT_TABLE0 to u64 pointer; points to first L2 Table
+     * |     + 32                                              // Address of the 32nd element of the L2 Table (32 is the last used element of the L2 Table)
+     * | )                                                     // Get the 32nd element of the L2 Table
+     * | & ~(0x03)                                             // Ignore TableDescriptorS1.{valid_bit, descriptor_type}
+     * | = 0x0000000040108000                                  // Pointer to last L3 Table
+     * 
+     * | *(
+     * |     (uint64_t*)(
+     * |         *(
+     * |             (uint64_t*)(
+     * |                 *(
+     * |                     (uint64_t*)&KERNEL_ROOT_TABLE0[0] // Address of the first element of KERNEL_ROOT_TABLE0
+     * |                 )                                     // Get the first element of KERNEL_ROOT_TABLE0
+     * |                 & ~(0x03)                             // Ignore TableDescriptorS1.{valid_bit, descriptor_type}
+     * |             )                                         // Cast first element of KERNEL_ROOT_TABLE0 to u64 pointer; points to first L2 Table
+     * |             + 32                                      // Address of the 32nd element of the L2 Table (32 is the last used element of the L2 Table)
+     * |         )                                             // Get the 32nd element of the L2 Table
+     * |         & ~(0x03)                                     // Ignore TableDescriptorS1.{valid_bit, descriptor_type}                       
+     * |     )                                                 // Cast 32nd element of L2 Table to u64 pointer; points to first L3 Table below 32nd L2 Table              
+     * |     + 66                                              // Address of the 66th element of the L3 Table (66 is the last used element of the L3 Table)                  
+     * | )                                                     // Get the 66th element of the L3 Table                
+     * | & ~(0x403)                                            // Ignore PageDescriptorS1.{valid_bit, descriptor_type, af}                        
+     * | = 0x0000000040108000                                  // Pointer to last physical page that's been used
+     * 
+     * So in other words, the last L3 Table in the page table tree is also the last physical page that's been used!
+     * Therefore, if we setup the page table such that the above is true, we'll have mapped all memory that's been 
+     * used by the kernel thus far (including the page table tree itself) into the page table tree. We would then be
+     * finally Boot👢strap🔫ped and ready to enable the MMU.                                    
+    */
+
     unsafe {
-        let mut cur_page: *const u8 = 0 as *const u8;
-        while cur_page < kernel_mem_end.sub(1) {
+        let kernel_mem_pages: u64 = ((kernel_mem_end as u64 - 1) / PAGE_LEN as u64) + 1;
+
+        let kernel_mem_end_l1_idx: u64 = get_bits(kernel_mem_end as u64, 46, 36);
+        let kernel_mem_end_l2_idx: u64 = get_bits(kernel_mem_end as u64, 35, 25);
+        let kernel_mem_end_l3_idx: u64 = get_bits(kernel_mem_end as u64, 24, 14);
+        if (kernel_mem_end_l1_idx >= 7)    || // TODO(chungmcl): verify/implement fix for this edge case:
+           (kernel_mem_end_l2_idx >= 2047) || // I think technically if l2_idx or l3_idx is >= 2047
+           (kernel_mem_end_l3_idx >= 2047) {  // I can just += 1 to pages_needed_for_page_table...?
+            panic!("");
+        }
+        let pages_needed_for_page_table: u64 =
+            kernel_mem_end_l1_idx + 1 // #(l2 tables allocated via get_page())
+            + 
+            kernel_mem_end_l2_idx + 1 // #(l3 tables allocated via get_page())
+            // l3 tables' entries are pointers to physical pages, so don't include in calculation
+        ;
+
+        let pages_to_map: u64 = kernel_mem_pages + pages_needed_for_page_table;
+        for pa in (0..pages_to_map * PAGE_LEN as u64).step_by(PAGE_LEN) {
+            let cur_page: *const u8 = pa as *const u8;
             match map_page_to_va(
                 &mut *(&raw mut KERNEL_ROOT_TABLE0), 
                 cur_page, 
@@ -48,7 +119,6 @@ pub fn bootstrap_kernel_page_tables(kernel_mem_end: *const u8) -> Result<(), PTM
                     return Err(e);
                 }
             }
-            cur_page = cur_page.add(PAGE_LEN);
         }
         return Ok(());
     }
